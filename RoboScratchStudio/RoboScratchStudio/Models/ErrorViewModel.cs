0530462563e05295cf1b8e6b3220bfefbb390db6
NQD23081 <nqd230899@gmail.com>
2024-08-26 22:02:01 +0700

    update tailwind template and config tailwind

---
+++ RoboScratchStudio/RoboScratchStudio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RoboScratchStudio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
