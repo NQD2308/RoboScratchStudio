b51b82ebb814181a23050394413fc6949c6a1a4c
NQD2308 <nqd230899@gmail.com>
2024-08-27 18:18:29 +0700

    build error page & update header footer

---
+++ RoboScratchStudio/RoboScratchStudio/wwwroot/js/carousel.js
@@ -0,0 +1,10 @@
+﻿let currentSlide = 1;
+const totalSlides = 3;
+
+function showNextSlide() {
+    document.getElementById('carousel-' + currentSlide).checked = false;
+    currentSlide = currentSlide % totalSlides + 1;
+    document.getElementById('carousel-' + currentSlide).checked = true;
+}
+
+setInterval(showNextSlide, 4000);
