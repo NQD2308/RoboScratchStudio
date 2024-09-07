cb06789b320ad9603284b9a8a5ee6e67546598ff
NQD2308 <nqd230899@gmail.com>
2024-09-07 18:10:41 +0700

    update all source

--- RoboScratchStudio/RoboScratchStudio/obj/Debug/net8.0/RoboScratchStudio.AssemblyInfo.cs
+++ RoboScratchStudio/RoboScratchStudio/obj/Debug/net8.0/RoboScratchStudio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoboScratchStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66b4442bdf1103d48c8f6d0fe242d5e464416aa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoboScratchStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoboScratchStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
